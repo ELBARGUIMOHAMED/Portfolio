be300e839247d314fbd4966e8dbe3bfc5d2e5d1b
ELBARGUI MOHAMED <barguim99@gmail.com>
2025-01-22 10:32:57 +0100

    Initial commit

---
+++ app/components/Work.jsx
@@ -0,0 +1,66 @@
+import { assets, workData } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+import { motion } from "motion/react"
+
+const Work = (isDarkMode) => {
+    return (
+        <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            id='work' className="w-full px-[12%] py-10 scroll-mt-20 ">
+            <motion.h4
+                initial={{ y: -20, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.3 }}
+                className="text-center mb-2 text-lg font-Ovo">My Portfolio</motion.h4>
+            <motion.h2
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.7 }}
+                className="text-center text-5xl font-Ovo">My Latest work</motion.h2>
+            <motion.p
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.7 }}
+                className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo ">Welcome to my web development portfolio. Explore a curated collection of projects that demonstrate my proficiency in both front-end and back-end development, as well as WordPress website creation </motion.p>
+            <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 0.6, delay: 0.9 }}
+                className='grid grid-cols-auto gap-5 my-10 dark:text-black '>
+                {workData.map((projects, index) => (
+                    <motion.div
+                        whileHover={{ scale: 1.05 }}
+                        transition={{ duration: 0.3 }}
+                        className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group' key={index} style={{ backgroundImage: `url(${projects.byImage})` }}>
+                        <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 
+                        py-3 px-5 flex items-center justify-between group-hover:bottom-7 duration-500'>
+                            <div>
+                                <h2 className='font-semibold ' >{projects.title}</h2>
+                                <p className='txt-sm text-gray-700'>{projects.description}</p>
+                            </div>
+                            <div className='border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition'>
+                                <Image src={assets.send_icon} alt='send icon' className='w-5' />
+                            </div>
+                        </div>
+
+                    </motion.div>
+                ))}
+            </motion.div>
+            <motion.a
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 1.1 }}
+                href='' className='w-max flex items-center justify-center gap-2 
+    text-gray-700 border-[0.5px] border-gray-700 rounded-full 
+    py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500 dark:text-white dark:broder-white dark:hover:bg-darkHover'>
+                Show More <Image src={isDarkMode ? assets.right_arrow_bold_dark : assets.right_arrow_bold} alt='Right arrow' className='w-4' />
+            </motion.a>
+
+        </motion.div>
+    )
+}
+
+export default Work
---
+++ assets/assets.js
@@ -0,0 +1,167 @@
+import user_image from './user-image.png';
+import code_icon from './code-icon.png';
+import code_icon_dark from './code-icon-dark.png';
+import edu_icon from './edu-icon.png';
+import edu_icon_dark from './edu-icon-dark.png';
+import project_icon from './project-icon.png';
+import project_icon_dark from './project-icon-dark.png';
+import vscode from './vscode.png';
+import firebase from './firebase.png';
+import wordpress from './wordpress.png';
+import js from './js.png';
+import laravel from './laravel.png';
+import html from './html.png';
+import nextjs from './nextjs.png';
+import php from './php.png';
+import css from './css.png';
+import tailwind from './tailwind.png';
+import react from './react.png';
+import figma from './figma.png';
+import git from './git.png';
+import mongodb from './mongodb.png';
+import right_arrow_white from './right-arrow-white.png';
+import logo from './logo.png';
+import logo_dark from './logo_dark.png';
+import mail_icon from './mail_icon.png';
+import mail_icon_dark from './mail_icon_dark.png';
+import profile_img from './profile-img.png';
+import download_icon from './download-icon.png';
+import hand_icon from './hand-icon.png';
+import header_bg_color from './header-bg-color.png';
+import moon_icon from './moon_icon.png';
+import sun_icon from './sun_icon.png';
+import arrow_icon from './arrow-icon.png';
+import arrow_icon_dark from './arrow-icon-dark.png';
+import menu_black from './menu-black.png';
+import menu_white from './menu-white.png';
+import close_black from './close-black.png';
+import close_white from './close-white.png';
+import web_icon from './web-icon.png';
+import mobile_icon from './mobile-icon.png';
+import ui_icon from './ui-icon.png';
+import graphics_icon from './graphics-icon.png';
+import right_arrow from './right-arrow.png';
+import send_icon from './send-icon.png';
+import right_arrow_bold from './right-arrow-bold.png';
+import right_arrow_bold_dark from './right-arrow-bold-dark.png';
+
+export const assets = {
+    user_image,
+    code_icon,
+    code_icon_dark,
+    laravel,
+    edu_icon,
+    edu_icon_dark,
+    project_icon,
+    project_icon_dark,
+    vscode,
+    css,
+    wordpress,
+    tailwind,
+    react,
+    firebase,
+    figma,
+    git,
+    nextjs,
+    php,
+    html,
+    js,
+    mongodb,
+    right_arrow_white,
+    logo,
+    logo_dark,
+    mail_icon,
+    mail_icon_dark,
+    profile_img,
+    download_icon,
+    hand_icon,
+    header_bg_color,
+    moon_icon,
+    sun_icon,
+    arrow_icon,
+    arrow_icon_dark,
+    menu_black,
+    menu_white,
+    close_black,
+    close_white,
+    web_icon,
+    mobile_icon,
+    ui_icon,
+    graphics_icon,
+    right_arrow,
+    send_icon,
+    right_arrow_bold,
+    right_arrow_bold_dark
+};
+
+export const workData = [
+    {
+        title: 'Website Development',
+        description: 'Web Design',
+        byImage: '/work-1.png',
+    },
+    {
+        title: 'Mobile App Development',
+        description: 'Mobile App',
+        byImage: '/work-2.png'
+    },
+    {
+        title: 'UI/UX Design',
+        description: 'UI/UX Design',
+        byImage: '/work-4.png'
+    },
+    {
+        title: 'Wordpress Development', 
+        description: 'wordpress ',
+        byImage: '/work-3.png'
+    },
+
+  
+
+]
+
+export const serviceData = [
+    { icon: assets.web_icon, title: 'Website Development', description: 'Website development is the process of building, programming, and maintaining websites to ensure functionality and aesthetics.', link: '' },
+    { icon: assets.mobile_icon, title: 'Mobile app', description: 'Mobile app development involves creating software applications for mobile devices, ensuring seamless performance and user experience.', link: '' },
+    { icon: assets.ui_icon, title: 'UI/UX design', description: 'UI/UX design focuses on creating a seamless and visually appealing user experience, enhancing user interaction with digital products.', link: '' },
+    { icon: assets.graphics_icon, title: 'WordPress Development', description: 'WordPress development specializes in building dynamic and customizable websites using the WordPress platform.', link: '' },
+
+]
+
+export const infoList = [
+    { 
+        icon: assets.code_icon, 
+        iconDark: assets.code_icon_dark, 
+        title: 'Languages', 
+        description: 'HTML, CSS, JavaScript, React.js, PHP, Laravel, WordPress, SQL, Python, Tailwind CSS' 
+    },
+    { 
+        icon: assets.edu_icon, 
+        iconDark: assets.edu_icon_dark, 
+        title: 'Education', 
+        description: 'Specialized Technician in Digital Development, OFPPT (Result: Excellent - 16.67)' 
+    },
+    { 
+        icon: assets.project_icon, 
+        iconDark: assets.project_icon_dark, 
+        title: 'Projects', 
+        description: 'Created multiple web development projects, including [Spouuu]https://spouuu.com/, focusing on responsive design and user experience' 
+    }
+];
+
+export const toolsData = [ 
+        assets.vscode,   // Visual Studio Code (IDE/Editor)
+        assets.html,     // HTML (Markup Language)
+        assets.css,      // CSS (Styling Language)
+        assets.php,      // PHP (Programming Language)
+        assets.laravel,  // Laravel (PHP Framework)
+        assets.js,       // JavaScript (Programming Language)
+        assets.react,    // React.js (JavaScript Library)
+        assets.nextjs,   // Next.js (React Framework)
+        assets.tailwind, // Tailwind CSS (CSS Framework)
+        assets.wordpress, // WordPress (CMS/Website Platform)
+        assets.git,      // Git (Version Control System)
+        assets.firebase, // Firebase (Backend-as-a-Service)
+        assets.figma     // Figma (UI/UX Design Tool)
+
+];
